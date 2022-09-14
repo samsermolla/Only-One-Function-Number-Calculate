529f56145c2333b2ea65341d7eb25583169e165a
Molla Samser <38918444+samsermolla@users.noreply.github.com>
2022-09-14 22:23:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let calculate = () => {
   num1 = document.getElementById("num1").value;
   num2 = document.getElementById("num2").value;
   add = parseInt(num1) + parseInt(num2);
-  //parse the values to int to be added otherwise values will be just concatenated and not added.
+  //parseInt the values to int to be added otherwise values will be just concatenated and not added.
   sub = num1 - num2;
   mult = num1 * num2;
   div = num1 / num2;
